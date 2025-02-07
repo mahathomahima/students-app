6da87069fb520cf1f00f82d37798b86492458b9b
Mahima <fams13013@gmail.com>
2025-02-07 10:55:28 +0530

    Updated code

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,15 +3,19 @@ import { Link } from "react-router-dom";
 import { signOut } from "firebase/auth";
 import { authentication } from "../../firebase";
 
+import './index.css'
+
 const Sidebar = ({ onLogout }) => {
   const onClickLogout = () => {
     signOut(authentication);
   }
 
   return (
-    <div className="sidebar">
+    <div className="dashboard">
+      <div className="sidebar">
       <Link to="/students">Students</Link>
       <button onClick={onClickLogout}>Logout</button>
+    </div>
     </div>
   );
 };
